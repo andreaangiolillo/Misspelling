9b4624677e103a6799e996247a5327c396e9c733
Work <Work@Andrew-PC>
2016-05-28 15:28:24 +0200

    CleanCsv @param @return
    ripulisce tutti i csv e li unisce in clean_tweets.csv

---
+++ Mispelling/src/main.py
@@ -0,0 +1,25 @@
+'''
+Created on 28 mag 2016
+
+@author: Work
+'''
+import tweetToCsv
+
+if __name__ == '__main__':
+    
+    #download tweet and put all in csv
+    
+   
+    """tweetToCsv.get_all_tweets("BBCBreaking")
+    tweetToCsv.get_all_tweets("POTUS")
+    tweetToCsv.get_all_tweets("nytimes")
+    tweetToCsv.get_all_tweets("WSJPolitics")
+    tweetToCsv.get_all_tweets("NBA")
+    tweetToCsv.get_all_tweets("SkyFootball")
+    tweetToCsv.get_all_tweets("Pontifex")  
+    tweetToCsv.get_all_tweets("WWF")
+    tweetToCsv.get_all_tweets("WSJ")
+    tweetToCsv.get_all_tweets("UN")
+    """
+    
+    tweetToCsv.cleanCsv()
